e837f25ac3025cc7f1138fbedf96292a19ff35dc
avinash1598 <madrupam@gmail.com>
2019-01-29 11:06:24 +0530

    close bullseye event

--- resources/views/bullseye.blade.php
+++ resources/views/bullseye.blade.php
@@ -44,6 +44,8 @@
 								<!-- Sidebar -->
 									<section id="sidebar">
 										<section>
+											<span class="button primary fit disabled">Event is over</span>
+											<br/><br/>
 											<!-- <a href="/bullseye-event" class="button primary fit">Play</a> -->
 											<!--a href="/bullseye-event" class="button primary fit">Play</a-->
 											<a href="/bullseye-event/leaderboard" class="button primary fit">Click to view results</a>
@@ -81,6 +83,7 @@
 											<li>There is absolutely no preparation for this game. All you need is free time and a lot of patience. Sometimes, obvious clues won’t strike you and at other times you might get deviated from your path by misinterpreting a clue. When this happens, take a break and play later.</li>
 											<li>The moderators are there to monitor the progress of players and glitches/bugs. An official clue tab will be available that gives a certain clue for each level and it will be updated as and when required.</li>
 											<li>Your attempts are tracked and unfair tactics will lead to disqualification. In case of such offenses, we will withhold the prize(s) for all those who were involved in breaking the rules.</li>
+											<li>The moderator's decision is final.</li>
 										</ul>
 
 										
