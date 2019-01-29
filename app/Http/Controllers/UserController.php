35292fe01306afeae5f137550fbd0b946862f3eb
avinash1598 <madrupam@gmail.com>
2019-01-29 14:56:51 +0530

    debug leaderboard logic

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -763,7 +763,7 @@ class UserController extends Controller
         $leaderboard = array();
 
         foreach ($frl as $usr) {
-            $leaderboard[$usr->cl] = (isset($leaderboard[$usr->cl])==true?$leaderboard[$usr->cl]:"").$usr->name." "; 
+            $leaderboard[$usr->cl] = "<div>".(isset($leaderboard[$usr->cl])==true?$leaderboard[$usr->cl]:"").$usr->name."</div>"; 
         }
 
         return view('bel',['frl' => $frl, 'rank' => $rank, '$userank' => $rank, 'user' => $userdetail, 'uf' => $uf, 'ind' => $ind,'leaderboard' => $leaderboard]);
