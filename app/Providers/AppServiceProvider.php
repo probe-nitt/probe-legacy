9b3f2a6a88199c0a202038a3366e4d93dc2f8161
Shohan <shohanduttaroy99@gmail.com>
2022-02-10 17:51:00 +0530

    Add config variable to toggle http protocol

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if(env('APP_ENV') === 'production') {
-            $this->app['request']->server->set('HTTPS', true);
+            if(env('APP_PROTOCOL') === 'HTTPS') {
+                $this->app['request']->server->set('HTTPS', true);
+            }
         }
     }
 
